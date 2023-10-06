b8f1aeb5f51186b6aa896ee7e48d07b109aeb69f
RomoloDeDominicis7 <131604254+RomoloDeDominicis7@users.noreply.github.com>
2023-10-06 12:09:45 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2,10 +2,8 @@ const canvas = document.getElementById("snakeCanvas");
 const context = canvas.getContext("2d");
 const boxSize = 20;
 const canvasSize = canvas.width;
-foodImg.src = 'apple.jpg';  // sostituisci 'apple.jpg' con il nome effettivo dell'immagine
-
 let foodImg = new Image();
-foodImg.src = 'path_to_your_image.jpg';  // sostituisci con il percorso effettivo dell'immagine nella tua cartella
+foodImg.src = 'apple.jpg';
 
 
 let snake = [{ x: 10 * boxSize, y: 10 * boxSize }];
